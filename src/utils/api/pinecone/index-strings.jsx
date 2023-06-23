ea621b694e19d3f824d75c883196cecd183433f7
Alana Golden <Alanalgolden@gmail.com>
2023-06-23 19:42:50 -0400

    pinecone link attempts, removing in next version

--- src/utils/api/pinecone/index-strings.jsx
+++ src/utils/api/pinecone/index-strings.jsx
@@ -7,7 +7,7 @@ import { getPineconeClient } from "./pinecone-config";
 const { createIndexIfNotExists, chunkedUpsert } = utils;
 
 const indexName = import.meta.env.VITE_PINECONE_INDEX;
-let counter = 0;
+let counter = 1;
 
 const run = async () => {
   //Get a Pinecone client instance
@@ -15,18 +15,20 @@ const run = async () => {
   const pineconeClient = await getPineconeClient();
 
   //Create a Pinecone index if it doesn't exist
-  await createIndexIfNotExists(pineconeClient, indexName, 384);
+  /* await createIndexIfNotExists(pineconeClient, indexName, 384); */
 
   //Select the target Pinecone index
   console.log("attempting to index client");
-  const index = pineconeClient.Index(indexName);
+  const index = pineconeClient;
+  console.log(index);
 
-  await embedder.init();
+  /*   await embedder.init();
   await embedder.embedBatch(["Hello World"], 1, async (embeddings) => {
+    console.log("attemping upsert");
     await chunkedUpsert(index, embeddings, "default");
   });
 
-  console.log(`Inserted ${counter} documents into the index ${indexName}`);
+  console.log(`Inserted ${counter} documents into the index ${indexName}`); */
 };
 
 export default run;
--- src/utils/api/pinecone/pinecone-config.jsx
+++ src/utils/api/pinecone/pinecone-config.jsx
@@ -16,26 +16,50 @@ import { PineconeClient } from "@pinecone-database/pinecone";
 
 const apiKey = import.meta.env.VITE_PINECONE_API_KEY;
 const environment = import.meta.env.VITE_PINECONE_ENVIRONMENT;
+const indexName = import.meta.env.VITE_PINECONE_INDEX;
 
-let pineconeClient = null || PineconeClient;
+/* const pinecone = new PineconeClient(); */
+
+/* export const getPineconeClient = async () => {
+  try {
+    console.log("trying getPineconeClient");
+    await pinecone.init({
+      environment: environment,
+      apiKey: apiKey,
+    });
+    const indexes = await pinecone.listIndexes();
+    console.log(indexes);
+    console.log("pineconeClient initialized");
+  } catch (e) {
+    console.log(`Error: ${e}`);
+  }
+}; */
+
+//This wasn't working, trying again
+let pineconeClient = null; //|| PineconeClient;
 
 export const getPineconeClient = async () => {
   //validateEnvironmentVariables();
+  console.log(apiKey);
+  console.log(environment);
+  console.log(indexName);
   try {
     console.log("trying getPineconeClient");
     if (pineconeClient) {
       console.log("It makes it here!");
       return pineconeClient;
     } else {
+      console.log("Went to ELSE");
       pineconeClient = new PineconeClient();
 
       await pineconeClient.init({
         apiKey: apiKey,
         environment: environment,
+        projectName: indexName,
       });
     }
     return pineconeClient;
   } catch (e) {
-    console.log(e);
+    console.log(`error in getPineconeClient: ${e}`);
   }
 };
